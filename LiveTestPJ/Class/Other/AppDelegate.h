9b6fa387fc191df040ad5673d92736cb096f358f
Macx <Macx@MacxdeSSSSSSSSSSSSSS.local>
2018-01-22 18:36:53 +0800

    Initial commit

---
+++ LiveTestPJ/Class/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveTestPJ
+//
+//  Created by Macx on 2018/1/22.
+//  Copyright © 2018年 Low-Key. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveTestPJ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LiveTestPJ
+//
+//  Created by Macx on 2018/1/22.
+//  Copyright © 2018年 Low-Key. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
